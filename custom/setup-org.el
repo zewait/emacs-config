8fa77422289ee4000044f0c070a20860ceeb87a1
世凡 黄 <773108248@qq.com>
2015-03-23 00:07:34 +0800

    add org-html-themes

--- custom/setup-org.el
+++ custom/setup-org.el
@@ -3,6 +3,9 @@
 ;;; code:
 (setq org-todo-keywords
 	  '((sequence "TODO(t)" "DOING(i!)" "HANGUP(h!)" "|" "DONE(d!)" "CANCEL(c!)")))
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((ditaa . t)))
 
 (provide 'setup-org)
 
